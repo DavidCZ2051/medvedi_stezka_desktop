222549f53f649182c7d8b1017f2c51ae548aa5fe
DavidCZ2051 <84991177+DavidCZ2051@users.noreply.github.com>
2024-05-23 14:47:24 +0200

    Fixed table scrolling

--- lib/routes/results.dart
+++ lib/routes/results.dart
@@ -57,115 +57,117 @@ class _ResultsState extends State<Results> {
                 },
               ),
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
+            Expanded(
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
-                child: DataTable(
-                  columnSpacing: 40,
-                  border: TableBorder.all(
-                    color: Colors.black,
-                  ),
-                  sortColumnIndex: 1,
-                  columns: const [
-                    DataColumn(
-                      label: Text("Umístění hlídky"),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: DataTable(
+                    columnSpacing: 40,
+                    border: TableBorder.all(
+                      color: Colors.black,
                     ),
-                    DataColumn(
-                      label: Text("Číslo hlídky"),
-                    ),
-                    DataColumn(
-                      label: Text("Příjmení a jméno"),
-                    ),
-                    DataColumn(
-                      label: Text("Rok narození"),
-                    ),
-                    DataColumn(
-                      label: Text("Jednota"),
-                    ),
-                    DataColumn(
-                      label: Text("Kategorie"),
-                    ),
-                    DataColumn(
-                      label: Text("Start"),
-                      tooltip: "H:MM:SS",
-                    ),
-                    DataColumn(
-                      label: Text("Cíl"),
-                      tooltip: "H:MM:SS",
-                    ),
-                    DataColumn(
-                      label: Text("Čas běhu"),
-                      tooltip: "H:MM:SS",
-                    ),
-                    DataColumn(
-                      label: Text("Trestné minuty celkem"),
-                      tooltip: "H:MM:SS",
-                    ),
-                    DataColumn(
-                      label: Text("Čekací doba celkem"),
-                      tooltip: "H:MM:SS",
-                    ),
-                    DataColumn(
-                      label: Text("Výsledný čas"),
-                      tooltip: "H:MM:SS",
-                    ),
-                  ],
-                  rows: <DataRow>[
-                    for (CompetitionCard card in search())
-                      DataRow(
-                        cells: <DataCell>[
-                          DataCell(
-                            Text("${selectedCompetition!.getPlace(card)}"),
-                          ),
-                          DataCell(
-                            Text("${card.team.number}"),
-                          ),
-                          DataCell(
-                            Text(
-                              "${card.team.members[0].firstName} ${card.team.members[0].lastName}\n${card.team.members[1].firstName} ${card.team.members[1].lastName}",
-                            ),
-                          ),
-                          DataCell(
-                            Text(
-                                "${card.team.members[0].birthYear}\n${card.team.members[1].birthYear}"),
-                          ),
-                          DataCell(
-                            Text(card.team.organization),
-                          ),
-                          DataCell(
-                            Text(card.team.category.toString()),
-                          ),
-                          DataCell(
-                            Text(formatTime(card.startSeconds)),
-                          ),
-                          DataCell(
-                            Text(formatTime(card.finishSeconds)),
-                          ),
-                          DataCell(
-                            Text(
-                              formatTime(
-                                  card.finishSeconds - card.startSeconds),
-                            ),
-                          ),
-                          DataCell(
-                            Text(
-                              formatTime(card.getTotalPenaltySeconds()),
-                            ),
-                          ),
-                          DataCell(
-                            Text(
-                              formatTime(card.getTotalWaitSeconds()),
-                            ),
-                          ),
-                          DataCell(
-                            Text(
-                              formatTime(card.getTotalSeconds()),
-                            ),
-                          ),
-                        ],
+                    sortColumnIndex: 1,
+                    columns: const [
+                      DataColumn(
+                        label: Text("Umístění hlídky"),
                       ),
-                  ],
+                      DataColumn(
+                        label: Text("Číslo hlídky"),
+                      ),
+                      DataColumn(
+                        label: Text("Příjmení a jméno"),
+                      ),
+                      DataColumn(
+                        label: Text("Rok narození"),
+                      ),
+                      DataColumn(
+                        label: Text("Jednota"),
+                      ),
+                      DataColumn(
+                        label: Text("Kategorie"),
+                      ),
+                      DataColumn(
+                        label: Text("Start"),
+                        tooltip: "H:MM:SS",
+                      ),
+                      DataColumn(
+                        label: Text("Cíl"),
+                        tooltip: "H:MM:SS",
+                      ),
+                      DataColumn(
+                        label: Text("Čas běhu"),
+                        tooltip: "H:MM:SS",
+                      ),
+                      DataColumn(
+                        label: Text("Trestné minuty celkem"),
+                        tooltip: "H:MM:SS",
+                      ),
+                      DataColumn(
+                        label: Text("Čekací doba celkem"),
+                        tooltip: "H:MM:SS",
+                      ),
+                      DataColumn(
+                        label: Text("Výsledný čas"),
+                        tooltip: "H:MM:SS",
+                      ),
+                    ],
+                    rows: <DataRow>[
+                      for (CompetitionCard card in search())
+                        DataRow(
+                          cells: <DataCell>[
+                            DataCell(
+                              Text("${selectedCompetition!.getPlace(card)}"),
+                            ),
+                            DataCell(
+                              Text("${card.team.number}"),
+                            ),
+                            DataCell(
+                              Text(
+                                "${card.team.members[0].firstName} ${card.team.members[0].lastName}\n${card.team.members[1].firstName} ${card.team.members[1].lastName}",
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                  "${card.team.members[0].birthYear}\n${card.team.members[1].birthYear}"),
+                            ),
+                            DataCell(
+                              Text(card.team.organization),
+                            ),
+                            DataCell(
+                              Text(card.team.category.toString()),
+                            ),
+                            DataCell(
+                              Text(formatTime(card.startSeconds)),
+                            ),
+                            DataCell(
+                              Text(formatTime(card.finishSeconds)),
+                            ),
+                            DataCell(
+                              Text(
+                                formatTime(
+                                    card.finishSeconds - card.startSeconds),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                formatTime(card.getTotalPenaltySeconds()),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                formatTime(card.getTotalWaitSeconds()),
+                              ),
+                            ),
+                            DataCell(
+                              Text(
+                                formatTime(card.getTotalSeconds()),
+                              ),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
                 ),
               ),
             ),
